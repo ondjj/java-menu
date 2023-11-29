f322e5853560203efd3ee471957aae4a9781ab21
Chang-Jun Jeon <jcjk0302@gmail.com>
2023-11-29 15:53:41 +0900

    feat(validator) : coach name 유효성 검사

--- src/main/java/menu/util/validation/CoachNameValidation.java
+++ src/main/java/menu/util/validation/CoachNameValidation.java
@@ -1,2 +1,16 @@
-package menu.util;public class CoachNameValidation {
+package menu.util.validation;
+
+import java.util.List;
+import menu.util.exception.IllegalArgumentExceptionType;
+
+public class CoachNameValidation {
+    public static void validateCoachNames(List<String> coachNames) {
+        if (hasInvalidNameLength(coachNames)) {
+            throw IllegalArgumentExceptionType.INVALID_NAME_LENGTH.getException();
+        }
+    }
+
+    private static boolean hasInvalidNameLength(List<String> coachNames) {
+        return coachNames.stream().anyMatch(name -> name.length() > 4 || name.length() < 2);
+    }
 }
